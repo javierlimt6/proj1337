c03e47c039ea08189a48e667fc32841dd366f329
Javier <javierlimt6@gmail.com>
2025-05-15 00:06:08 +0800

    solved 0138.py

--- med/0138.py
+++ med/0138.py
@@ -13,11 +13,33 @@ class Solution:
         #add each node to a list for easy retrieval
         #loop again to assign random pointers
         #space O(n) time O(n)
-
+        
         # new = new Node(head.val, head.next, )
         # cur = head
         # while cur.next:
 
         #FAILED
 
-        pass
+        #attempt 2:
+        # loop through original list to make hashmap, oldnode:newnode with val done, next & random not done
+        # loop original list again, from each original node find the new node, update next & random using hashmap
+
+        nodemap = {None:None} #handle nulls 
+
+        cur = head
+        #first loop
+        while cur:
+            nodemap[cur] = Node(cur.val)
+            cur = cur.next
+
+
+        #second loop
+        #now we add in next and random
+        cur = head
+        while cur:
+            new = nodemap[cur]
+            new.next = nodemap[cur.next]
+            new.random = nodemap[cur.random]
+            cur = cur.next
+        
+        return nodemap[head]
